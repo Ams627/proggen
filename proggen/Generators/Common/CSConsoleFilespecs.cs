f9f255a27cbde2d22e6e9b3c77e393b21eb6d06b
Adrian Sims <adriansims62781@gmail.com>
2019-10-02 21:03:16 +0200

    added .net core 3.0 project

--- proggen/Generators/Common/CSConsoleFilespecs.cs
+++ proggen/Generators/Common/CSConsoleFilespecs.cs
@@ -162,4 +162,56 @@ namespace Proggen.Generators.Common
             new FileSpec (CommonFileSpecs.GitIgnore)
         };
     }
+
+    static class NetCore3FileSpecs
+    {
+        private static string sixteen = new string(' ', 16);
+        private static string errorStuff =
+            sixteen + "var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;\n" +
+            sixteen + "var progname = Path.GetFileNameWithoutExtension(fullname);\n" +
+            sixteen + "Console.Error.WriteLine(progname + \": Error: \" + ex.Message);";
+
+        public static FileSpec[] NetCore3Specs =
+        {
+            new FileSpec {
+                Pathname = "$$(PROJECTNAMECAMEL)/Program.cs",
+                Contents = new [] {
+                    "\uFEFF" + // prepended BOM
+                    "using System;",
+                    "using System.Collections.Generic;",
+                    "using System.IO;",
+                    "using System.Linq;",
+                    "using System.Text;",
+                    "using System.Threading.Tasks;\n",
+                    "namespace $$(PROJECTNAMECAMEL)",
+                    "{",
+                    "    class Program",
+                    "    {",
+                    "        static void Main(string[] args)",
+                    "        {",
+                    $"            try\n            {{\n                //startstarttypingtypingherehere\n            }}\n            catch (Exception ex)\n            {{\n{errorStuff}\n            }}\n",
+                    "        }",
+                    "    }",
+                    "}"
+                }
+            },
+            new FileSpec {
+                Pathname = "$$(PROJECTNAMECAMEL)/$$(PROJECTNAMECAMEL).$$(SUFFIX)",
+                Contents = new [] {
+                    "\uFEFF" + // prepended BOM
+                    "<Project Sdk=\"Microsoft.NET.Sdk\">",
+                    "",
+                    "  <PropertyGroup>",
+                    "    <OutputType>Exe</OutputType>",
+                    "    <TargetFramework>netcoreapp3.0</TargetFramework>",
+                    "  </PropertyGroup>",
+                    "",
+                    "</Project>",
+                    "",
+                }
+            },
+            new FileSpec (CommonFileSpecs.SlnFileSpec),
+            new FileSpec (CommonFileSpecs.GitIgnore)
+        };
+    }
 }
---
+++ proggen/Generators/NetCore3/NetCore3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Proggen.Generators.Common;
+
+namespace Proggen
+{
+    [AutoRegister]
+    class NetCore3 : ProgramGenerator
+    {
+        public override string Name => "netcore3";
+
+        public override string Description => "Generate a .Net Core 3 console application for Visual Studio 2019.";
+
+        public override string VSVersion => "2019";
+
+        public override string PlatformToolset => "v142";
+
+        public override Guid ProjectTypeGUID => ProjectTypeGUIDs.csGuid;
+
+        public override string SolutionConfig => "Any CPU";
+
+        public override string ProjectSuffix => "csproj";
+
+        public override string Command => "OpenCSharpConApp";
+
+        public override string CommandParam => "";
+
+        public override FileSpec[] FileSpecs => Proggen.Generators.Common.NetCore3FileSpecs.NetCore3Specs;
+
+        public override List<string> Folders => null;
+    }
+}
